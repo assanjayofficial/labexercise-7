29d3bc22d9e901df30d6ac8d6a9ab36e12698273
assanjayofficial <80383032+assanjayofficial@users.noreply.github.com>
2021-04-19 21:48:28 +0800

    Update index.js

--- src/components/atoms/Gap/index.js
+++ src/components/atoms/Gap/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-
 const Gap = ({height}) => {
-  return <View style={styles.gap(height)} />;
+  return <View style={styles.gap(height)}/>;
 };
-
 export default Gap;
 
 const styles = StyleSheet.create({
-  gap: height => ({
-    height: height,
+  gap: height => ({ height: height,
   }),
 });
